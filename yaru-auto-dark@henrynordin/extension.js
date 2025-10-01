1cfdd8ce17c5625ff12472302490c7892d587716
HenryNordin <59847084+HenryNordin@users.noreply.github.com>
2025-10-01 23:33:26 +0200

    Add support for icon and legacy applications theming

--- yaru-auto-dark@henrynordin/extension.js
+++ yaru-auto-dark@henrynordin/extension.js
@@ -3,12 +3,12 @@ import { Extension } from 'resource:///org/gnome/shell/extensions/extension.js';
 
 export default class DarkModeThemeExtension extends Extension {
     enable() {
-        // Create GSettings object to manage the user's GNOME Shell theme
+        // Manage GNOME Shell theme
         this._userThemeSettings = new Gio.Settings({
             schema_id: 'org.gnome.shell.extensions.user-theme'
         });
 
-        // Create GSettings object to read interface settings, such as dark mode preference
+        // Manage icon and GTK theme
         this._interfaceSettings = new Gio.Settings({
             schema_id: 'org.gnome.desktop.interface'
         });
@@ -37,7 +37,35 @@ export default class DarkModeThemeExtension extends Extension {
         // 0 = default/light, 1 = prefer dark
         const darkMode = this._interfaceSettings.get_enum('color-scheme') === 1;
 
-        const theme = darkMode ? 'Yaru-dark' : 'Yaru';
-        this._userThemeSettings.set_string('name', theme);
+        // Update shell theme dynamically
+        let shellTheme = this._userThemeSettings.get_string('name');
+        if (!shellTheme) shellTheme = 'Adwaita';
+        this._userThemeSettings.set_string('name', this._getVariant(shellTheme, darkMode));
+
+        // Update GTK and icon themes dynamically
+        const gtkTheme = this._interfaceSettings.get_string('gtk-theme');
+        if (gtkTheme) {
+            this._interfaceSettings.set_string('gtk-theme', this._getVariant(gtkTheme, darkMode));
+        }
+
+        const iconTheme = this._interfaceSettings.get_string('icon-theme');
+        if (iconTheme) {
+            this._interfaceSettings.set_string('icon-theme', this._getVariant(iconTheme, darkMode));
+        }
+    }
+
+    /**
+     * Generate the dark or light variant of a theme by appending or removing '-dark'.
+     * This works with Yaru or any theme following the '*-dark' naming convention.
+     */
+    _getVariant(themeName, darkMode) {
+        if (darkMode) {
+            if (!themeName.toLowerCase().endsWith('-dark')) {
+                return `${themeName}-dark`;
+            }
+        } else {
+            return themeName.replace(/-dark$/i, '');
+        }
+        return themeName;
     }
 }
